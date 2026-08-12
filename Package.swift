ecd288efbcdb863a8e170b82dc05b04eaafbb75a
Levi McCallum <levi@levimccallum.com>
2026-08-12 15:03:21 -0700

    Publish corrected tvOS engine artifact

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let iosLibgodotTarget: Target = .binaryTarget(
 
 let tvosLibgodotTarget: Target = .binaryTarget(
     name: "tvos_libgodot",
-    url: "https://github.com/levi/godot/releases/download/v4.7.1-basis3/libgodot-tvos.xcframework.zip",
-    checksum: "51a659f2c6e868148f4f732e3e775317e12c345e0ff32d974a2971756b8e064c"
+    url: "https://github.com/levi/godot/releases/download/v4.7.1-basis4/libgodot-tvos.xcframework.zip",
+    checksum: "60b7e59dd0dcccf39eac8768a1cc0dcf78c554a4fbd8de09df3ed09cb1a63731"
 )
 
 let package = Package(
